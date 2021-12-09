655f96013aa14aef7f1d1ab0378b850353251874
shintapatricia <192111270@students.mikroskil.ac.id>
2021-12-09 22:54:57 +0700

    Pertemuan 7 - Penggunaan atribut URL pada request

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ var http = require("http");
 
 http
   .createServer((req, res) => {
-    res.write("<p>Pengembangan Aplikasi Web dengan Nodejs</p>");
-    res.end();
+    if (req.url == "/home") {
+      res.write("<p>Konten Home</p>");
+    } else if (req.url == "/product") {
+      res.write("<p>Konten Product</p>");
+    } else if (req.url == "/order") {
+      res.write("<p>Konten Order</p>");
+    } else if (req.url == "/") {
+      res.write("<p><Pengembangan Aplikasi Web dengan Node.js</p>");
+    } else {
+      res.end();
+    }
   })
   .listen(8000, () => {
     console.log("Server is running on port 8000");
