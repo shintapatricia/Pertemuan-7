6b219730db49cfe6a7523eb7ac2aec3720f0f34b
shintapatricia <192111270@students.mikroskil.ac.id>
2021-12-09 23:38:53 +0700

    Pertemuan 7 - json, post, get, bodyParser

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 var http = require("http");
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var persons = [];
+
+app.post("/person", (req, res) => (persons.push(req.body), res.json(req.body)));
+
+app.get("/person", (req, res) => res.json(persons));
+// var data = [];
+// app.get("/data", (req, res) => {
+//   res.json(data);
+// });
+
+// app.post("/data", (req, res) => {
+//   data.push(Date.now());
+//   res.json(data);
+// });
 
 http
-  .createServer((req, res) => {
-    if (req.url == "/home") {
-      res.write("<p>Konten Home</p>");
-    } else if (req.url == "/product") {
-      res.write("<p>Konten Product</p>");
-    } else if (req.url == "/order") {
-      res.write("<p>Konten Order</p>");
-    } else if (req.url == "/") {
-      res.write("<p><Pengembangan Aplikasi Web dengan Node.js</p>");
-    } else {
-      res.end();
-    }
-  })
-  .listen(8000, () => {
-    console.log("Server is running on port 8000");
-  });
+  .createServer(app)
+  .listen(8000, () => console.log("Server is running on port 8000"));
